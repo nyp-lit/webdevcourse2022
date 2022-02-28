0f199ff48372940818a8332e8e50497ae7e318d9
Jolene <jolenechong7@gmail.com>
2022-02-28 13:11:48 +0800

    added day 3 starter code and updated README

---
+++ Day 3 Advanced Javascript/starter/script.js
@@ -0,0 +1,47 @@
+// SECTION 1 created by Sharina
+// SECTION 2 created by Jolene Chong
+
+// CODE FOR SECTION 1
+
+
+// Q1) Convert ”932” (a string) into a number using a type conversion method
+
+// Q2) Now make another variable b and set the value as “h”, use another method to convert the value into a number (Hint: Use the unary + operator)
+
+// Q2a) Console.log the variable b. What is its data type?
+
+// Q3) Round off the floating point number 2.71828 to 2 decimal places.
+
+// ARROW FUNCTIONS Q1) Rewrite the following code to arrow syntax:
+function Multiplication(a, b) {
+  return a * b;
+}
+
+function Hello(name) {
+  return 'Hello' + name;
+}
+
+function Farewell() {
+  return 'Bye';
+}
+function CalculateGST(price) {
+  const onepercent = price / 100
+  return onepercent * 7
+}
+
+// END OF CODE FOR SECTIOn 1
+
+
+
+// CODE FOR SECTION 2
+
+
+// function to loadPosts() from data/users.json db
+
+// function to wait for DOM to load then asynchronously add a post and display posts after
+
+// using APIs with async await
+
+
+
+// END OF CODE FOR SECTION 2
